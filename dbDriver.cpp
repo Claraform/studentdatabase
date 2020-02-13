c911cccfd3e8419bc425426f692e5ade3692e10e
Claraform <stassenclara@gmail.com>
2020-02-13 09:07:55 +0200

    Altered driver program to make use of a namespace

--- dbDriver.cpp
+++ dbDriver.cpp
@@ -2,13 +2,12 @@
 #include <iostream>
 #include <cstdlib>
 
-void clear(void){
-	system("clear");
-}
+namespace STSCLA001{
+	void clear(void){
+		system("clear");
+	}
 
-int main(){
-	char select;
-	for (;;){
+	void menu (){
 		std::cout << "0: Add student" << std::endl;
 		std::cout << "1: Read database" << std::endl;
 		std::cout << "2: Save database" << std::endl;
@@ -16,35 +15,43 @@ int main(){
 		std::cout << "4: Grade student" << std::endl;
 		std::cout << "q: Quit" << std::endl;
 		std::cout << "Enter a number (or q to quit) and press return..." << std::endl;
-
-		std::cin >> select; //read user input for database option
-
-		if (select == '0'){
-			std::cout << "function addStudent() called" << std::endl;
-		}
-		else if(select == '1'){
-			std::cout << "function readDatabase() called" << std::endl;
-		}
-		else if(select == '2'){
-			std::cout << "function saveDatabase() called" << std::endl;
-		}
-		else if(select == '3'){
-			std::cout << "function displayData() called" << std::endl;
-		}
-		else if(select == '4'){
-			std::cout << "function gradeStudent() called" << std::endl;
-		}
-		else if(select == 'q'){
-			std::cout << "Program exiting..." << std::endl;
-			break;
-		}
-		else{
-			std::cout << "Invalid option... exiting" << std::endl;
-			//system exits
-			break;
-		}
-		clear();
 	}
+
+
+}
+
+int main(){
+	char select;	
+	for (;;){
+		STSCLA001::menu();
 	
+	std::cin >> select; //read user input for database option
+
+	if (select == '0'){
+		std::cout << "function addStudent() called" << std::endl;
+	}
+	else if(select == '1'){
+		std::cout << "function readDatabase() called" << std::endl;
+	}
+	else if(select == '2'){
+		std::cout << "function saveDatabase() called" << std::endl;
+	}
+	else if(select == '3'){
+		std::cout << "function displayData() called" << std::endl;
+	}
+	else if(select == '4'){
+		std::cout << "function gradeStudent() called" << std::endl;
+	}
+	else if(select == 'q'){
+		std::cout << "Program exiting..." << std::endl;
+		break;
+	}
+	else{
+		std::cout << "Invalid option... exiting" << std::endl;
+		//system exits
+		break;
+	}
+	STSCLA001::clear();
+	}
 	return 0;
 }
