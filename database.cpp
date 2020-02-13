58f681d5bf494064bf23006bbc88062131d833c5
Claraform <stassenclara@gmail.com>
2020-02-13 10:01:51 +0200

    Code compiles without library issues

--- database.cpp
+++ database.cpp
@@ -1,1 +1,19 @@
 //database
+
+#include "database.h"
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+namespace STSCLA001{
+	extern std::vector<StudentRecord> studentData;
+	//function definitions
+	void add_student(std::string name, std::string surname, std::string snumber, std::string crecord);
+	void readDatabase(std::string filename);
+	void saveDatabase(std::string filename);
+	void displayData(std::string snumber);
+	void gradeStudent(std::string snumber);
+}
--- database.h
+++ database.h
@@ -1,5 +1,7 @@
 #ifndef DATABASE_H
 #define DATABASE_H
+#include <string>
+#include <vector>
 
 namespace STSCLA001{
 	struct StudentRecord{
